f26b8e5518e81071608b58a4d954fbb7e9593ca6
Ingrid Wong <yw340@georgetown.edu>
2018-01-14 03:13:17 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -49,5 +49,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.map{|word, i| word.concat("s") if i != 1}
+  array.each_with_index.map {|word, i| word.concat("s") if i != 1}
 end
